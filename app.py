e2facf63e8e15e8616adb732373f130ec4cf0248
Dibya Das <dibyadascool@gmail.com>
2017-10-05 19:12:48 +0530

    init

---
+++ app.py
@@ -0,0 +1,16 @@
+from flask import Flask, redirect, url_for, request, render_template, send_from_directory
+
+app = Flask(__name__)
+
+@app.route('/image')
+def root():
+	print("pinged!!")
+	# return "opened"
+	return (send_from_directory(directory="file", filename="img.png", as_attachment=False),200,{'Access-Control-Allow-Origin': '*'})
+	
+
+
+if __name__ == "__main__":
+	# port = int(os.environ.get("PORT", 3433))
+	app.run(host='localhost', port=3433,debug=True)
+
---
+++ content.js
@@ -0,0 +1,35 @@
+//alert("Hi from read-it");
+
+// var firstHref = $("a[href^='http']").eq(0).attr("href");
+
+// console.log(firstHref);
+
+//console.log($("#testc").html());
+
+
+alert("Hi from read-it");
+
+InboxSDK.load(1, 'sdk_dibyadas_b332aae32c').then(function(sdk){
+
+  // the SDK has been loaded, now do something with it!
+  sdk.Compose.registerComposeViewHandler(function(composeView){
+
+    // a compose view has come into existence, do something with it!
+    //var send = document.querySelector('div.editable[role="textbox"][aria-label="Message Body"]');
+    //console.log(send);
+    // send.innerHTML = '';
+    // var sendHandler = function() {
+    // 	console.log("clicked send!");
+    // 	var msg;
+    // 	console.log()
+    // };
+    //send.addEventListener('click', sendHandler, false);
+    composeView.on('presending', function(event){
+    	var msg = composeView.getBodyElement().innerHTML;
+    	composeView.getBodyElement().innerHTML = '';
+        // this.insertHTMLIntoBodyAtCursor("<head><meta http-equiv='Content-Security-Policy' content='img-src  https://016ff7a5.ngrok.io/image'> </meta></head>"+"<br>"+msg+"<img src='https://016ff7a5.ngrok.io/image' />");
+        this.insertHTMLIntoBodyAtCursor(msg+"<img crossorigin='anonymous' src='https://d3-force-directed.herokuapp.com/image' />");
+  	});
+
+	});
+});
